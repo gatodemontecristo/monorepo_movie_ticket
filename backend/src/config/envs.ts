28879aed18556c7200e48a6fabab673f82cabff1
gatodemontecristo <erickdevicente2024@outlook.com>
2025-09-16 12:28:11 -0500

    ci: added dynamic backend and frontend ports

--- backend/src/config/envs.ts
+++ backend/src/config/envs.ts
@@ -4,5 +4,5 @@ import * as env from 'env-var';
 export const envs = {
   DATABASE_URL: env.get('DATABASE_URL').required().asString(),
   JWT_SECRET: env.get('JWT_SECRET').required().asString(),
-  PORT: env.get('PORT').required().asPortNumber(),
+  PORT: env.get('BACKEND_PORT').required().asPortNumber(),
 };
